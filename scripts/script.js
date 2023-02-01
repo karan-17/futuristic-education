826dbb5b2e252cc801fea329a10cd67efdcc67c7
karan <karanvaswani2002@gmail.com>
2023-02-01 23:36:34 +0530

    project prototype update

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+let item = document.querySelectorAll(".item");
+
+item.forEach((x) => x.addEventListener("click", itemFunction))
+
+function itemFunction() {
+    this.classList.toggle("active")
+}
+
+function navSlide() {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+    
+    burger.addEventListener("click", () => {
+        //Toggle Nav
+        nav.classList.toggle("nav-active");
+        
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle("toggle");
+    });
+    
+}
+
+navSlide();
